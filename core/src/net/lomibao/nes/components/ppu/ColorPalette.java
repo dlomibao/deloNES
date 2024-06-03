6c002315d1649d192fb6b7a6be820b67e6d8b1f2
lomibaod <lomibaod@autonation.com>
2024-06-02 22:08:27 -0400

    add libgdx colors

--- core/src/net/lomibao/nes/components/ppu/ColorPalette.java
+++ core/src/net/lomibao/nes/components/ppu/ColorPalette.java
@@ -1,14 +1,19 @@
 package net.lomibao.nes.components.ppu;
 
+import com.badlogic.gdx.graphics.Color;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+
 @Log4j2
 public class ColorPalette {
     private String paletteName;
     private int[][] palette;//[color index][0,1,2=red,green,blue]
+    private List<Color> colorList;//libgdx colors
+
     public ColorPalette(InputStream inputStream, String name){
         this.paletteName=name;
         load(inputStream);
@@ -24,6 +29,9 @@ public class ColorPalette {
                     palette[idx][c]=Byte.toUnsignedInt(data[c]);
                 }
                 log.info("{} :: ({},{},{})",idx,palette[idx][0],palette[idx][1],palette[idx][2]);
+
+                Color color=new Color(palette[idx][0]/255.0f,palette[idx][1]/255.0f,palette[idx][2]/255.0f,1.0f);
+                colorList.add(color);
                 idx++;
             }
         }catch (Exception e){
